5b489482f01c076a425081b3c104683e807ef20d
jaedb <james@barnsley.co.nz>
2014-01-24 16:01:07 +1300

    MultiClassGridField up and running. Dependant on gridfieldextensions module however.

--- code/Admin/ModuleManager.php
+++ code/Admin/ModuleManager.php
@@ -3,27 +3,9 @@
 class ModuleManager extends DataObject {
 
 	public function getCMSFields() {
-		// construct the field container
-		$fields = FieldList::create( $tabSet = TabSet::create('Root'));
-		
+		$fields = FieldList::create( $tabSet = TabSet::create('Root'));		
 		return $fields;
 	}
-	
-	/* add a save button (if we decide to use this dataobject
-	public function getCMSActions() {
-		if (Permission::check('ADMIN') || Permission::check('EDIT_SITECONFIG')) {
-			$actions = new FieldList(
-				FormAction::create('save_siteconfig', _t('CMSMain.SAVE','Save'))
-					->addExtraClass('ss-ui-action-constructive')->setAttribute('data-icon', 'accept')
-			);
-		} else {
-			$actions = new FieldList();
-		}
-		
-		$this->extend('updateCMSActions', $actions);
-		
-		return $actions;
-	}*/
 	
 	public function setRequest($req){
 		return false;
